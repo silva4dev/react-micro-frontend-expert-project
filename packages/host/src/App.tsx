09b6f5be4e9f86fc4638ec3aec9a8e395db82226
silva4dev <web4dev.lucas@outlook.com>
2025-01-10 21:54:14 -0300

    feat: ✨ add App

---
+++ packages/host/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Header from 'header/header';
+
+const App = () => (
+  <>
+    <Header />
+    <div className="p-20">
+      <p className="text-2xl text-red-500 font-bold">Host remote!</p>
+    </div>
+  </>
+);
+
+export default App;
